d65f684b8a279351d065bbd3d3baa0cfabcbd605
Massimo2006 <padmass2006@gmail.com>
2021-06-26 23:37:27 -0400

    Add project files.

---
+++ Engine V 01/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* common C++ Stuff */
+
+
+/* Windows stuff */
+#include "Defines.h"
+
+/* DirectX Stuff */
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"DirectXTK.lib")
+
+
